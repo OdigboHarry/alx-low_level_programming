e31f27a94d199f276b26b2666ade61cacabb563e
OdigboHarry <odigoharry@gmail.com>
2023-03-21 00:30:49 +0100

    Added file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints all letters of the alphabets
+ * except q and e using putchar
+ *
+ * Return: 0 Always(Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'e' || c == 'q')
+		{
+			c++;
+		}
+		else
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
