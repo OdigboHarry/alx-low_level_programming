abfbc35381b82434c93b8169ddcddb991aafbb8e
OdigboHarry <odigoharry@gmail.com>
2023-03-21 02:02:53 +0100

    Added file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - A program that prints all possible combinations of single
+ * digit numbers separated by a comman and space using putchar
+ *
+ * Return: 0 Always(Success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+		putchar('0' + c);
+		if (c < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar('$');
+		}
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
