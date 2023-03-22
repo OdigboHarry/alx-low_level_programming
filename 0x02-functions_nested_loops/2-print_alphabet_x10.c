084f90c28cf5911382bda85331fdec03a2ee4d2d
OdigboHarry <odigoharry@gmail.com>
2023-03-23 00:01:18 +0100

    Removed function parameter description

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,13 +4,11 @@
 /**
  * print_alphabet_x10 - A function that prints all letters of the alphabets
  * ten times
- * @c: holds the character that is printed out
- * @d: variable for count
  * Return: void
  */
 void print_alphabet_x10(void)
 {
-	char c = 'a';
+	int c = 'a';
 	int d = 0;
 
 	while (d < 10)
