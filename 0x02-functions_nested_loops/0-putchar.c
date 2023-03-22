4ab2aba034c3ba80690303842957de6afe16df38
OdigboHarry <odigoharry@gmail.com>
2023-03-22 19:35:31 +0100

    Added file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints _putchar using the 
+ * function _putchar
+ *
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	_putchar();
+	
+	return (0);
+}
