749ef1ed0097ba92c3489fbbf63c473ede67d10e
OdigboHarry <odigoharry@gmail.com>
2023-03-23 02:02:04 +0100

    Added file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - A function that prints the absolute value of an integer
+ * @n: formal parameter that holds the arguments of the actual parameter from
+ * the main function
+ * Return: 0 Always(Success)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
