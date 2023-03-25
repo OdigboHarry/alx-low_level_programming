bdf12d0068794d8ad7149f2c8f85f07dff7e2370
OdigboHarry <odigoharry@gmail.com>
2023-03-25 02:41:52 +0100

    Added condition for size of triangle to be 0 or less than 0

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,18 +9,25 @@ void print_triangle(int size)
 {
 	int c, d, e, f;
 
-	f = size - 1;
-	for (c = 1; c <= size; c++)
+	if (size == 0 || size < 0)
 	{
-		for (d = 0; d < f; d++)
-		{
-			_putchar(' ');
-		}
-		for (e = 0; e < c; e++)
-		{
-			_putchar('#');
-		}
-		f--;
 		_putchar('\n');
+	}
+	else
+	{
+		f = size - 1;
+		for (c = 1; c <= size; c++)
+		{
+			for (d = 0; d < f; d++)
+			{
+				_putchar(' ');
+			}
+			for (e = 0; e < c; e++)
+			{
+				_putchar('#');
+			}
+			f--;
+			_putchar('\n');
+		}
 	}
 }
