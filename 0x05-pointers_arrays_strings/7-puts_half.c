52ba3de9b08f68889e7e91d049a62ac840dcb83b
OdigboHarry <odigoharry@gmail.com>
2023-04-02 01:03:49 +0100

    Added condition for empty string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,30 +8,37 @@
 void puts_half(char *str)
 {
 	int c, d, n, e;
-	
+
 	d = 0;
 	for (c = 0; str[c] != '\0'; c++)
 	{
 		d++;
 	}
 	e = d;
-	if (d % 2 == 0)
+	if (str[0] == '\0')
 	{
-		e = d / 2;
-		for (; e <= d; e++)
-		{
-			_putchar(str[e]);
-		}
 		_putchar('\n');
 	}
-	else if(d % 2 == 1)
+	else
 	{
-		n = (d - 1) / 2;
-		n++;
-		for (; str[n] != '\0'; n++)
+		if (d % 2 == 0)
 		{
-			_putchar(str[n]);
+			e = d / 2;
+			for (; e < d; e++)
+			{
+				_putchar(str[e]);
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
+		else if (d % 2 == 1)
+		{
+			n = (d - 1) / 2;
+			n++;
+			for (; str[n] != '\0'; n++)
+			{
+				_putchar(str[n]);
+			}
+			_putchar('\n');
+		}
 	}
 }
