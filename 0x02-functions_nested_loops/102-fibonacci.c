93e4ccdc425ecd6c80f22c3f21ff2f03cd13b8e1
OdigboHarry <odigoharry@gmail.com>
2023-07-08 17:01:53 +0100

    Added file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints 50 fibonacci numbers starting from 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int a, b, c, d;
+
+	a = 1, b = 0, c = 0, d = 0;
+
+	while (d < 50)
+	{
+		printf("%ld", c);
+		c = a + b;
+		if (d == 49)
+		{
+			printf("\n");
+			break;
+		}
+		else
+		{
+			printf(", ");
+		}
+		a = b;
+		b = c;
+		d++;
+	}
+	return (0);
+}
